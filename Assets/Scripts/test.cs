b703a9cb1ce239a40bd834f0e5bfbe7284607fde
Quinn Moore <127361464+Quinny450@users.noreply.github.com>
2025-03-01 00:07:24 -0800

    General code cleanup

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -1,10 +1,18 @@
+using System;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class test : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+        public String sceneName;
+        public bool isNextScene = true;
+
+        [SerializeField]
+        public SceneInfo sceneInfo;
+    void OnTriggerEnter2D(Collider2D collision)
     {
+        sceneInfo.isNextScene = isNextScene;
         SceneManager.LoadScene("Level_2_Alpha");
     }
 }
