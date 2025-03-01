00ab82d7417520c562e866c4ac9e5caac412ef9c
Quinn Moore <127361464+Quinny450@users.noreply.github.com>
2025-03-01 00:14:56 -0800

    Fixed PC drift, rearranged levels

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -5,14 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class test : MonoBehaviour
 {
-        public String sceneName;
-        public bool isNextScene = true;
-
-        [SerializeField]
-        public SceneInfo sceneInfo;
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        sceneInfo.isNextScene = isNextScene;
         SceneManager.LoadScene("Level_2_Alpha");
     }
 }
